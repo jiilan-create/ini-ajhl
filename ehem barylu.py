5c7501648e4fb65ff12863f7ab1227f5cd723feb
jiilan-create <66149604+jiilan-create@users.noreply.github.com>
2020-06-06 12:04:37 +0700

    Add files via upload

---
+++ ehem barylu.py
@@ -0,0 +1,10 @@
+import sys,time
+def jalan (kata):
+    for e in kata:
+       sys.stdout.write(e)
+       sys.stdout.flush()
+       time.sleep(0.1)
+jalan("Goblok lu beraninya keroyokan dan dibelakang".center(44))
+jalan("nih buat lu bansat".center(44))
+jalan(" .YOUR CPU THAT IS IN YOUR SMART PHONE HAS BEEN HACKED".center(44))
+jalan("thank you motherfuckers because of you I can control your device".center
